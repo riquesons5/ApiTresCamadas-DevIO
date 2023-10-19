ddc208aafb1349fb2491376204a1c5bf4c798e36
Henrique Quevedo <henrique.quevedo@useall365.onmicrosoft.com>
2023-10-19 11:47:56 -0300

    Finalizando o módulo de desenvolvimento da camada de negócios

---
+++ src/DevIO.Business/Interfaces/IFornecedorRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IFornecedorRepository : IRepository<Fornecedor>
+    {
+        Task<Fornecedor> ObterFornecedorEndereco(Guid id);
+        Task<Fornecedor> ObterFornecedorProdutoEndereco(Guid id);
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+        Task RemoverEnderecoFornecedor(Endereco endereco);
+    }
+}
---
+++ src/DevIO.Business/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DevIO.Business.Models
+{
+    public class Produto : Entity
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+    }
+}

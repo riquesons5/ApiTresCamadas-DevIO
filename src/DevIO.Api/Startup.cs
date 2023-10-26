2e745a1bfd5f9c81d16ae0ddb0a6e2ba2b40c51b
Henrique Quevedo <henrique.quevedo@useall365.onmicrosoft.com>
2023-10-26 13:56:37 -0300

    Implementando repositorios e view models

--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -18,7 +18,6 @@ namespace DevIO.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -34,7 +33,6 @@ namespace DevIO.Api
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
---
+++ src/DevIO.Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,43 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Data.Context;
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Repository
+{
+    public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
+    {
+        public FornecedorRepository(MeuDbContext db) : base(db)
+        {
+        }
+
+        public async Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(c => c.Endereco)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Fornecedor> ObterFornecedorProdutoEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(c => c.Produtos)
+                .Include(c => c.Endereco)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+        {
+            return await Db.Enderecos.AsNoTracking()
+                .FirstOrDefaultAsync(f => f.FornecedorId == fornecedorId);
+        }
+
+        public async Task RemoverEnderecoFornecedor(Endereco endereco)
+        {
+            Db.Enderecos.Remove(endereco);
+            await SaveChanges();
+        }
+    }
+}
